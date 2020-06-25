861a87bf72a9385fd672ef416490be5a25ed1a1f
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-06-25 15:43:52 -0300

    Organizes the files and folders. Insert the nunjucks template for all pages

---
+++ jsClass - CSS, HTML and JavaScript/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS e CSS",
+        duration: "50 min",
+        price: "FREE"
+    },
+    {
+        id: "x4FdZd2-_uU",
+        title: "Recriando Interface do Discord",
+        duration: "2h 22min",
+        price: "FREE"
+    },
+    {
+        id: "7A4UQGrFU9Q",
+        title: "Iniciando no ReactJS",
+        duration: "10 min",
+        price: "FREE"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid",
+        duration: "35 min",
+        price: "FREE"
+    },
+    {
+        id: "MW7hrQe6aYo",
+        title: "Iniciando com Git",
+        duration: "6 min",
+        price: "FREE"
+    },
+    {
+        id: "gx50Kv6-2KA",
+        title: "Iniciando Vagrant",
+        duration: "10 min",
+        price: "FREE"
+    }
+]
--- jsClass - CSS, HTML and JavaScript/server.js
+++ jsClass - CSS, HTML and JavaScript/server.js
@@ -3,30 +3,81 @@ const server = express()
 
 const nunjucks = require("nunjucks")
 
+const videos = require("./data")
+
 server.use(express.static('public/styles'))
 server.use(express.static('public/assets'))
 server.use(express.static('src/scripts'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
-nunjucks.configure("views", { express: server })
-
+nunjucks.configure("views", { 
+    express: server,
+    autoescape: false 
+})
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/63468436?s=460&u=3fe614e09a9f62c607788347afda48c129750cac&v=4",
+        name: "Abner Pena de Souza",
+        role: 'Support Engineer in - <a href="http://dropreal.com/" target="_blank">DropReal</a>',
+        school: 'Student in - <a href="http://rocketseat.com.br/" target="_blank">Rocketseat</a>',
+        office: "Web Developer Enthusiast",
+        links: [
+            { name: "Github", url: "https://github.com/abnerpsouza99" },
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/abnerpenadesouza/"}
+        ]
+    }
+
+
+    return res.render("about", {about})
 })
 
 server.get("/classes", function(req, res){
-    return res.render("classes")
+    return res.render("classes", { items: videos})
 })
 
 server.get("/contents", function(req, res){
-    return res.render("contents")
+
+    let contents = [
+        {
+            id: "starter",
+            image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+            title: "Starter",
+            modules: "5 módulos",
+            price: "FREE"
+        },
+        {
+            id: "launchbase",
+            image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+            title: "LaunchBase",
+            modules: "7 fases com diversos módulos",
+            price: "R$960,00"
+        },
+        {
+            id: "gostack",
+            image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+            title: "GoStack",
+            modules: "10 fases com diversos módulos",
+            price: "R$1.000,00"
+        }
+    ]
+
+
+    return res.render("contents", {contents: contents})
 })
 
 server.get("/aboutRocketseat", function(req, res){
     return res.render("aboutRocketseat")
 })
+
+server.get("/not-found", function(req, res){
+    return res.render("not-found")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 
 // Comand to turn-on server
 server.listen(5000, function(){
